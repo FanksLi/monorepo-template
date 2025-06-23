548719b7d719148884a826d387c503243a1f4578
fanksli <1059098842@qq.com>
2025-06-23 23:18:59 +0800

    项目初始化

---
+++ apps/react-main/index.js
@@ -0,0 +1,4 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var lib_1 = require("@fan/lib");
+console.log((0, lib_1.add)(1, 2));
---
+++ apps/react-main/index.ts
@@ -0,0 +1,4 @@
+import { add } from '@fan/lib';
+
+
+console.log(add(1, 2));
